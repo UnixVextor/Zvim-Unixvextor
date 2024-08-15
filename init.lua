5a8e28196a1034f96db657478696b27df0050fdf
unixvextor <withun.wch@gmail.com>
2024-08-15 19:05:19 +0700

    initial commit

--- init.lua
+++
@@ -1,2 +0,0 @@
-require("unixvextor.core")
-require("unixvextor.lazy")
--- lua/unixvextor/core/init.lua
+++
@@ -1,4 +0,0 @@
--- import config of neovim
-require('unixvextor.core.option')
-require('unixvextor.core.globals')
-require('unixvextor.core.keymaps')
