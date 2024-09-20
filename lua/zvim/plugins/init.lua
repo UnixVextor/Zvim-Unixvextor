42122b84b05a0c5f5223cee06929bfb451fed5a5
unixvextor <withun.wch@gmail.com>
2024-09-20 18:44:57 +0700

    add: whick-key plugin

--- lua/zvim/plugins/init.lua
+++ lua/zvim/plugins/init.lua
@@ -243,5 +243,15 @@ return {
       "MunifTanjim/nui.nvim",
       "rcarriga/nvim-notify",
     }
-  }
+  },
+
+  {
+    "folke/which-key.nvim",
+    keys = { "<leader>", "<c-r>", "<c-w>", '"', "'", "`", "c", "v", "g" },
+    cmd = "WhichKey",
+    config = function(_, opts)
+      dofile(vim.g.base46_cache .. "whichkey")
+      require("which-key").setup(opts)
+    end,
+  },
 }
